76e03e92d88ab0b002e766e3e548e8a9618d9efa
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-03-21 13:57:00 +0800

    Eslint formatting

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,20 +1,21 @@
-import { watch, WatchListener } from "fs-extra";
-import { BASE_DIR } from "./directories";
+import { watch, WatchListener } from 'fs-extra';
+import { BASE_DIR } from './directories';
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 const PREPROCESSOR_FLAG = '-P';
-const WATCH_FLAG = '--watch'
+const WATCH_FLAG = '--watch';
 if (args.includes(PREPROCESSOR_FLAG) && args.includes(WATCH_FLAG)) {
-    // For now, running every second will do...
-    const run = () => {
-        console.clear();
-        require('./preprocess').main()
-        console.log('Running preprocessor...')
-    }
+  // For now, running every second will do...
+  const run = () => {
+    console.clear();
+    require('./preprocess')
+      .main();
+    console.log('Running preprocessor...');
+  };
 
-    setInterval(run, 1000);
-
+  setInterval(run, 1000);
 } else if (args.includes(PREPROCESSOR_FLAG)) {
-    require('./preprocess').main();
+  require('./preprocess')
+    .main();
 }
