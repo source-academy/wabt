ec2c971bb3954fe679de2184b765d896815949e0
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-04-07 12:50:42 +0800

    Speed up jest tests with esbuild

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+//   globals: {
+//     "ts-jest": {
+//       isolatedModules: true,
+//     }
+//   }
+// };
+
+// Make ts tests like, 5x faster https://stackoverflow.com/questions/45087018/jest-simple-tests-are-slow
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  transform: {
+    "^.+\\.(t|j)sx?$": ["esbuild-jest"],
+  }
+}
