f6a4e53050a1646717f3d2ca7b16b31557db711d
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-04-12 20:31:43 +0800

    Settle merge

--- src/opcode.cpp.ts
+++ src/opcode.cpp.ts
@@ -1,156 +1,81 @@
-import { OpcodeType } from "./opcode"
-import { Type } from "./type";
+/* eslint-disable @typescript-eslint/no-use-before-define */
+// / export enum OpcodeType {
+// /     #define WABT_OPCODE(rtype, type1, type2, type3, mem_size, prefix, code, Name, \
+// /     text, decomp) \
+// /     Name,
+// /         #include "wabt/opcode.def"
+// /     #undef WABT_OPCODE
+// /     Invalid,
+// / };
 
-///#if 0
-    `
-///#endif
-// Opcode enumerations.
-//
-// NOTE: this enum does not match the binary encoding.
-//
-export enum OpcodeType {
-    #define WABT_OPCODE(rtype, type1, type2, type3, mem_size, prefix, code, Name, \
-    text, decomp) \
-    Name = code,
-    #include "wabt/opcode.def"
-    #undef WABT_OPCODE
-    Invalid = 'invalid',
-};
-///#if 0
-`
-///#endif
+// we need non-type imports after preprocessing
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { ValueType } from './common/type';
+import { opcodeData, type OpcodeType } from './opcode';
 
-// // Static opcode objects.
-// #define WABT_OPCODE(rtype, type1, type2, type3, mem_size, prefix, code, Name, \
-//     text, decomp)                                             \
-// export const Opcode Name##_Opcode;
-// #include "wabt/opcode.def"
-// #undef WABT_OPCODE
+export namespace Opcode {
+  type DataType = {
+    rtype: ValueType;
+    type1: ValueType;
+    type2: ValueType;
+    type3: ValueType;
+    mem_size: number;
+    prefix: number;
+    code: number;
+    Name: OpcodeType;
+    text: string;
+    decomp: string;
+  };
 
-class Opcode {
-
-    constructor() { }
-    static constructor1(e: OpcodeType) {
-        this.enum_ = e;
-    }
-
-    Enum(): operator {
-        return this.enum_;
-    }
-
-    static FromCode(uint32_t): Opcode;
-    static FromCode(uint8_t prefix, uint32_t code): Opcode;
-    HasPrefix(): boolean { return GetInfo().prefix != 0; }
-    GetPrefix(): number { return GetInfo().prefix; }
-    GetCode(): number { return GetInfo().code; }
-    GetLength(): number { return GetBytes().size(); }
-    GetName(): string { return GetInfo().name; }
-    GetDecomp(): string {
-        return * GetInfo().decomp ? GetInfo().decomp : GetInfo().name;
-    }
-Type GetResultType() const { return GetInfo().result_type; }
-Type GetParamType1() const { return GetInfo().param_types[0]; }
-Type GetParamType2() const { return GetInfo().param_types[1]; }
-Type GetParamType3() const { return GetInfo().param_types[2]; }
-Type GetParamType(int n) const { return GetInfo().param_types[n - 1]; }
-Address GetMemorySize() const { return GetInfo().memory_size; }
-
-// Get the byte sequence for this opcode, including prefix.
-std:: vector < uint8_t > GetBytes() const ;
-
-// Get the lane count of an extract/replace simd op.
-uint32_t GetSimdLaneCount() const ;
-
-// Return 1 if |alignment| matches the alignment of |opcode|, or if
-// |alignment| is WABT_USE_NATURAL_ALIGNMENT.
-bool IsNaturallyAligned(Address alignment) const ;
-
-// If |alignment| is WABT_USE_NATURAL_ALIGNMENT, return the alignment of
-// |opcode|, else return |alignment|.
-Address GetAlignment(Address alignment) const ;
-
-static bool IsPrefixByte(uint8_t byte) {
-    return byte == kMathPrefix || byte == kThreadsPrefix || byte == kSimdPrefix;
+  export function getReturnType(o: OpcodeType): ValueType {
+    return opcodeData[o][0];
+  }
+  export function getParamTypes(o: OpcodeType): ValueType[] {
+    const params = [
+      opcodeData[o][1],
+      opcodeData[o][2],
+      opcodeData[o][3],
+    ].filter((p) => p !== ValueType.___);
+    return params;
+  }
+  export function getParamLength(o: OpcodeType): number {
+    return getParamTypes(o).length;
+  }
+  export function getMemSize(o: OpcodeType): ValueType {
+    return opcodeData[o][4];
+  }
+  export function getPrefix(o: OpcodeType): number {
+    return opcodeData[o][5];
+  }
+  export function getCode(o: OpcodeType): number {
+    return opcodeData[o][6];
+  }
+  export function getText(o: OpcodeType): string {
+    return opcodeData[o][7];
+  }
+  export function getDecompText(o: OpcodeType): string {
+    return opcodeData[o][8];
+  }
 }
 
-bool IsEnabled(const Features& features) const ;
-bool IsInvalid() const { return enum_ >= Invalid; }
-
-private:
-static constexpr uint32_t kMathPrefix = 0xfc;
-static constexpr uint32_t kThreadsPrefix = 0xfe;
-static constexpr uint32_t kSimdPrefix = 0xfd;
-
-struct Info {
-    const char* name;
-    const char* decomp;
-  Type result_type;
-  Type param_types[3];
-  Address memory_size;
-  uint8_t prefix;
-  uint32_t code;
-  uint32_t prefix_code;  // See PrefixCode below. Used for fast lookup.
-};
-
-static uint32_t PrefixCode(uint8_t prefix, uint32_t code) {
-    if (code >= (1 << MAX_OPCODE_BITS)) {
-        // Clamp to (2^bits - 1), since we know that it is an invalid code.
-        code = (1 << MAX_OPCODE_BITS) - 1;
-    }
-    return (prefix << MAX_OPCODE_BITS) | code;
-}
-
-// The Opcode struct only stores an enumeration (Opcode::Enum) of all valid
-// opcodes, densely packed. We want to be able to store invalid opcodes as
-// well, for display to the user. To encode these, we use PrefixCode() to
-// generate a uint32_t of the prefix/code pair, then negate the value so it
-// doesn't overlap with the valid enum values. The negation is done using
-// `~code + 1` since prefix_code is unsigned, and MSVC warns if you use - on
-// an unsigned value.
-//
-// | 0             | Opcode::Invalid         | INT32_MAX+1    UINT32_MAX |
-// |---------------|-------------------------|---------------------------|
-// | valid opcodes |      unused space       |      invalid opcodes      |
-//
-static Enum EncodeInvalidOpcode(uint32_t prefix_code) {
-  Enum result = static_cast<Enum>(~prefix_code + 1);
-    assert(result >= Invalid);
-    return result;
-}
-
-static void DecodeInvalidOpcode(Enum e,
-    uint8_t * out_prefix,
-    uint32_t * out_code) {
-  uint32_t prefix_code = ~static_cast<uint32_t>(e) + 1;
-  * out_prefix = prefix_code >> MAX_OPCODE_BITS;
-  * out_code = prefix_code & 0xff;
-}
-
-Info GetInfo() const ;
-static Info infos_[];
-
-Enum enum_;
-};
-
-// static
-inline Opcode Opcode:: FromCode(uint32_t code) {
-    return FromCode(0, code);
-}
-
-// static
-inline Opcode Opcode:: FromCode(uint8_t prefix, uint32_t code) {
-uint32_t prefix_code = PrefixCode(prefix, code);
-
-    if (WABT_LIKELY(prefix_code < WABT_ARRAY_SIZE(WabtOpcodeCodeTable))) {
-  uint32_t value = WabtOpcodeCodeTable[prefix_code];
-        // The default value in the table is 0. That's a valid value, but only if
-        // the code is 0 (for nop).
-        if (WABT_LIKELY(value != 0 || code == 0)) {
-            return Opcode(static_cast<Enum>(value));
-        }
-    }
-
-    return Opcode(EncodeInvalidOpcode(prefix_code));
-}
-
-}  // namespace wabt
+// / export const opcodeData: Record<
+// / OpcodeType,
+// / [
+// /   ValueType,
+// /   ValueType,
+// /   ValueType,
+// /   ValueType,
+// /   number,
+// /   number,
+// /   number,
+// /   string,
+// /   string,
+// / ]
+// / > = {
+// /    #define WABT_OPCODE(rtype, type1, type2, type3, mem_size, prefix, code, Name, \
+// /    text, decomp) \
+// /    [OpcodeType.Name]: [ValueType.rtype, ValueType.type1, ValueType.type2, ValueType.type3, mem_size, prefix, code, text, decomp],
+// /        #include "wabt/opcode.def"
+// /    #undef WABT_OPCODE
+// /   [OpcodeType.Invalid]: [ValueType.___, ValueType.___, ValueType.___, ValueType.___, 0, 0, 0, '', ''],
+// / };
--- test/wat_compiler/resources/module_program_fragments.ts
+++ test/wat_compiler/resources/module_program_fragments.ts
@@ -380,7 +380,7 @@ const function_exports_by_name: ModuleTestCase = {
 
 export const moduleTestCases = [
   module_with_one_simple_add_function_with_param_names,
-  // module_with_exported_add_function_no_names,
-  // module_with_two_exports,
-  // function_exports_by_name,
+  module_with_exported_add_function_no_names,
+  module_with_two_exports,
+  function_exports_by_name,
 ];
