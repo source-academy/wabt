dee6db6d06536bd60b2b7b31dbb5cecc5c771b03
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-05-10 16:56:40 +0800

    Add signed leb128 encoding functions

--- src/utils/number_encoder.ts
+++ src/utils/number_encoder.ts
@@ -37,6 +37,46 @@ function unsigned_bigint_to_leb128(value: bigint) {
   return new Uint8Array(result);
 }
 
+function signed_number_to_leb128(value: number) {
+  const negative = value < 0;
+  const result: number[] = [];
+  do {
+    let byte = value & 0x7f;
+    value >>= 7;
+    if (
+      (value === 0 && (byte & 0x40) === 0)
+      || (value === -1 && (byte & 0x40) !== 0)
+    ) {
+      // more bytes to come
+      byte |= 0x80;
+    }
+
+    result.push(byte);
+  } while (value !== 0);
+
+  return new Uint8Array(result);
+}
+
+function signed_bigint_to_leb128(value: bigint) {
+  const negative = value < 0;
+  const result: number[] = [];
+  do {
+    let byte = Number(value & BigInt(0x7f));
+    value >>= BigInt(7);
+    if (
+      (value === BigInt(0) && (byte & 0x40) === 0)
+      || (value === BigInt(-1) && (byte & 0x40) !== 0)
+    ) {
+      // more bytes to come
+      byte |= 0x80;
+    }
+
+    result.push(byte);
+  } while (value !== BigInt(0));
+
+  return new Uint8Array(result);
+}
+
 // function u32_to_leb128
 // function i32_to_leb128
 // function u64_to_leb128
@@ -45,4 +85,6 @@ function unsigned_bigint_to_leb128(value: bigint) {
 export const NUMBER_ENCODER_TEST_EXPORTS = {
   unsigned_number_to_leb128,
   unsigned_bigint_to_leb128,
+  signed_number_to_leb128,
+  signed_bigint_to_leb128,
 };
