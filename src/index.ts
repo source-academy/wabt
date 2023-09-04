21e79189d37f5ed8a0ff1e126a7fb5deca73412c
Yongbeom-Kim <dernbu@gmail.com>
2023-09-04 16:09:25 +0800

    v1.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import { ParseTree } from './wat_compiler/tree_types';
-import { compile as _compileParseTree, parse } from './wat_compiler';
-import { getSingleToken } from './wat_compiler/lexer';
-import { Tree } from './wat_compiler/tree_types';
+// import { ParseTree } from './wat_compiler/tree_types';
+// import { getSingleToken } from './wat_compiler/lexer';
+// import { Tree } from './wat_compiler/tree_types';
 
 /**
  * Compile a given WebAssembly Binary Text module into a binary.
  * @param program program to compile.
  * @returns an 8-bit integer array.
  */
-const compile: (program: string) => Uint8Array = (program: string) => _compileParseTree(parse(program));
+// const compile: (program: string) => Uint8Array = (program: string) => _compileParseTree(parse(program));
 
 /**
  * Get the string representation of the parse tree of a given WebAssembly Binary Text expression.
  * @param program program to parse
  * @returns a tree of string tokens.
  */
-const getStringParseTree: (program: string) => Tree<string> = (
-  program: string,
-) => ParseTree.getStringArrayRepr(parse(program));
+// const getStringParseTree: (program: string) => Tree<string> = (
+//   program: string,
+// ) => ParseTree.getStringArrayRepr(parse(program));
 
 /**
  * Compile a given parse tree of a given WebAssembly Binary Text expression.
@@ -26,14 +25,14 @@ const getStringParseTree: (program: string) => Tree<string> = (
  * @param tree tree to compile.
  * @returns an 8-bit integer array.
  */
-const compileParseTree: (tree: Tree<string> | ParseTree) => Uint8Array = (
-  tree: Tree<string> | ParseTree,
-) => {
-  if (!(tree instanceof ParseTree)) {
-    tree = Tree.treeMap(tree, getSingleToken); // TODO implement decompiler then compile to get token metadata
-  }
-  return _compileParseTree(tree);
-};
+// const compileParseTree: (tree: Tree<string> | ParseTree) => Uint8Array = (
+//   tree: Tree<string> | ParseTree,
+// ) => {
+//   if (!(tree instanceof ParseTree)) {
+//     tree = Tree.treeMap(tree, getSingleToken); // TODO implement decompiler then compile to get token metadata
+//   }
+//   return _compileParseTree(tree);
+// };
 
-export { parse } from './wat_compiler';
-export { compile, getStringParseTree, compileParseTree };
+export { parse, getIR, compile } from './wat_compiler';
+// export { getStringParseTree, compileParseTree };
--- src/wat_compiler/index.ts
+++ src/wat_compiler/index.ts
@@ -1,6 +1,7 @@
+import { ValueType } from '../common/type';
 import { BinaryWriter } from './binary_writer';
-import { getIR } from './ir';
-import { type ModuleExpression } from './ir_types';
+import { getIR as _getIR } from './ir';
+import { FunctionSignature, ImportExpression, type ModuleExpression } from './ir_types';
 import { tokenize } from './lexer';
 import { getParseTree } from './parser';
 import { type ParseTree } from './tree_types';
@@ -11,8 +12,31 @@ import { type ParseTree } from './tree_types';
  * @returns a tree of string tokens.
  */
 export const parse = (program: string) => getParseTree(tokenize(program));
-export const compile = (parseTree: ParseTree) => {
-  const ir = getIR(parseTree) as ModuleExpression;
+export const getIR = (parseTree: ParseTree) => _getIR(parseTree);
+export const compile = (ir: ModuleExpression) => {
   const binaryWriter = new BinaryWriter(ir);
   return binaryWriter.encode();
+};
+
+/*
+  Functions for dynamic imports
+*/
+export const I32 = ValueType.I32;
+export const I64 = ValueType.I64;
+export const F32 = ValueType.F32;
+export const F64 = ValueType.F64;
+
+// trunk-ignore(eslint/@typescript-eslint/no-unused-vars)
+export const createFunctionImport = (importModule: string, importName: string, functionName: string, paramTypes: ValueType[], returnTypes: ValueType[]) => {
+  for (const type of paramTypes) {
+    if (type !== I32 && type !== I64 && type !== F32 && type !== F64) {
+      throw new Error(`Invalid function param type: Expected I32, I64, F32, or F64. Got: ${type}`);
+    }
+  }
+  for (const type of returnTypes) {
+    if (type !== I32 && type !== I64 && type !== F32 && type !== F64) {
+      throw new Error(`Invalid function return type: Expected I32, I64, F32, or F64. Got: ${type}`);
+    }
+  }
+  return ImportExpression.functionImport(importModule, importName, new FunctionSignature(functionName, null, paramTypes, [null], returnTypes, [], []));
 };
